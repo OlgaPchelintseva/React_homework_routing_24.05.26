15bd8a8f6a23dde63ee861d62d5612475495bcad
Olga Pchelintseva <gxjkrf998@yandex.ru>
2026-05-24 20:27:42 +0700

    modification NotFound

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from 'react-router-dom';
+import logo from '../Layout/logo.png';
+import './NotFound.css';
 
 function NotFound(){
     const navigate = useNavigate();
     const handleGoHome = () => {navigate('/shop');};
     return (
-        <>
+        <div className='errorBox'>
             <h1>Ошибка 404</h1>
             <p>Этот поворот оказался тупиковым. Включаем задний ход и ищем новый путь!</p>
-            <button onClick={handleGoHome}>Вернуться на главную</button>
-        </>
+            <button onClick={handleGoHome} className='btnError'>Вернуться на главную</button>
+            <img src={logo}/>
+        </div>
     )
 };
 
